809958e81ebf756162a9bbf4a2162603b2d8e832
Shady Hussein <shadyhussein99@gmail.com>
2023-04-17 08:32:20 +0200

    validating child form

--- src/ChildForm.jsx
+++ src/ChildForm.jsx
@@ -8,14 +8,14 @@ function ChildForm(props) {
         <label className="col-span-1">{props.label}</label>
         <input className="col-span-1" placeholder="Enter fees in AED" type="number" {...props.register(props.feesInputName, {onChange: props.feesChange})}/>
 
-        {props.errors[props.registerFeesName] && (
-            <p className="text-red-500">{props.errors[props.registerFeesName].message}</p>)}
+        {props.errors[props.feesInputName] && (
+            <p className="text-red-500">{props.errors[props.feesInputName].message}</p>)}
 
         <p className="col-span-1 m-auto text-lg font-semibold">{props.sumInsured}</p>
         <input className="col-span-1" type="text" {...props.register(props.schoolInputName, {onChange: props.schoolChange})}/>
 
-        {props.errors[props.registerSchoolName] && (
-            <p className="text-red-500">{props.errors[props.registerSchoolName].message}</p>)}
+        {props.errors[props.schoolInputName] && (
+            <p className="text-red-500">{props.errors[props.schoolInputName].message}</p>)}
 
     </div>
 }
